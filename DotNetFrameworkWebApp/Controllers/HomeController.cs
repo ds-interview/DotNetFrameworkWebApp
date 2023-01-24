6177f4d792fefda893443d256de8873c36855805
arnav <arnav@JAGATPAL>
2023-01-24 10:43:38 +0530

    Commit

--- DotNetFrameworkWebApp/Controllers/HomeController.cs
+++ DotNetFrameworkWebApp/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace DotNetFrameworkWebApp.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Your application description page Index.";
 
             return View();
         }
